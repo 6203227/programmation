51d49c64624250d4bd24ad6a7a8d130350877199
Edemm007 <2219953@bdeb.qc.ca>
2022-09-26 15:54:29 -0400

    inverse
    inverser la première et la dernière lettre

--- testfile.py
+++ testfile.py
@@ -8,3 +8,11 @@ print("test 6")
 input("comment allez vous")
 print("test 4")
 print("aiaiaiaiiai")
+s= str(input("Entrer votre mot:"))
+list_s =list(s)
+n = len(s)
+first = s[0]
+last  = s[n-1]
+s1 = s[1:n-1]
+s2 = last + s1 + first
+print(s2)
