36aad17b44cfef21ccbf7b6d566a7deccb580621
6203227 <114160764+6203227@users.noreply.github.com>
2022-10-04 14:16:45 -0400

    ab

--- file-maitre.py
+++ file-maitre.py
@@ -6,18 +6,6 @@ print("test 6")
 input("comment allez vous")
 print("test 4")
 print("aiaiaiaiiai")
-s= str(input("Entrer votre mot:"))
+s = str(input("Entrer votre mot:"))
 
-<<<<<<< HEAD:testfile.py
-list_s =list(s)
-n = len(s)
-first = s[0]
-last  = s[n-1]
-s1 = s[1:n-1]
-s2 = last + s1 + first
-print(s2)
-=======
-print("test mau")
 
-# test Anton final
->>>>>>> 6f9cb82c6b5258bce834d19a8a263457dffc2231:file-maitre.py
--- mauricio.py
+++ mauricio.py
@@ -1,2 +1,15 @@
 # Hi, did it worked?
 print("test mau final")
+
+<<<<<<< HEAD:testfile.py
+list_s =list(s)
+n = len(s)
+first = s[0]
+last  = s[n-1]
+s1 = s[1:n-1]
+s2 = last + s1 + first
+print(s2)
+=======
+print("test mau")
+
+>>>>>>> 6f9cb82c6b5258bce834d19a8a263457dffc2231:file-maitre.py
